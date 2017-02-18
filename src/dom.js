74290945cc628d7252330879a19994b93dae5538
mokro1 <wm1609658889@163.com>
2017-02-18 19:32:35 +0800

    添加了appendTo方法

--- src/dom.js
+++ src/dom.js
@@ -22,6 +22,21 @@ Wad.parseHTML = function ( html ) {
     return arr;  
 };
 
+// 简单优化
+    Wad.fn.appendTo = function ( arr ) {
+        // this, 是伪数组, 要加的是 this[ 0 ]
+        // 将其加到 arr[ i ] 中
+        var tmp = [], tmpNode;
+        for ( var j = 0; j < this.length; j++ ) {
+            for ( var i = 0; i < arr.length; i++ ) {
+                tmpNode = i == arr.length - 1 ? this[ j ] : this[ j ].cloneNode( true );
+                tmp.push( tmpNode );
+                arr[ i ].appendChild( tmpNode );
+            }
+        }
+        return tmp; // 最终要返回的是 Wad 对象
+    }
+
 
 
 })( window );
